01c8dadfa33d7549dc72f50a765edb646d78655e
Michael Witrant <michael@ouvrages-web.fr>
2013-03-26 11:38:26 +0100

    moved assets outside source

--- config.rb
+++ config.rb
@@ -53,6 +53,9 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+set :js_assets_paths, ['assets/javascripts']
+set :css_assets_paths, ['assets/stylesheets']
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
